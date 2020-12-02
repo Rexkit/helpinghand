67650c68422822ce4aac2a9feb531ed370a79361
Nikita Kuzin <ptznikko@gmail.com>
2020-12-03 00:35:50 +0300

    updating header UI

--- client/src/containers/Header/index.jsx
+++ client/src/containers/Header/index.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import SearchField from '../../components/SearchField'
-import './header.css';
+import SearchField from '../../components/SearchField';
+import Button from '../../components/Button';
+import styles from './header.module.css';
+import AccountIcon from '../../assets/icons/account-icon.png';
 
 export default class Header extends Component {
     render() {
         return (
-            <Container className="header styled-box">
+            <Container className={`${styles.header} styled-box`}>
                 <Row>
                     <Col>
                         <h1>Helping Hand</h1>
@@ -16,8 +18,11 @@ export default class Header extends Component {
                     <Col>
                         <SearchField />
                     </Col>
-                    <Col>
-                        <SearchField />
+                    <Col className={styles.account_block}>
+                        <div className={styles.account_icon}>
+                            <img src={AccountIcon} alt=""/>
+                        </div>
+                        <Button buttonType="primary">Login</Button>
                     </Col>
                 </Row>
             </Container>
