c0a79b329b54a509cce6d6da6d84eb3c65f3bacf
Nikita Kuzin <ptznikko@gmail.com>
2020-12-07 22:56:02 +0300

    Adding components for other pages

---
+++ client/src/containers/CreateRequestPage/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateRequestPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>CreateRequestPage</h1>
+            </div>
+        )
+    }
+}
