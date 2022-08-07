fd7466cba7895058af68a6dd484b1f52f764347c
Nikita <ptznikko@gmail.com>
2022-08-07 12:37:50 +0300

    adding basic code spliting based on routes

--- client/src/core/Routes.jsx
+++ client/src/core/Routes.jsx
@@ -1,23 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
 import { connect } from 'react-redux';
-import MainPage from '../containers/MainPage';
-import ProtectedRoute from './ProtectedRoute';
-import RequestPage from '../containers/RequestPage';
-import ProfilePage from '../containers/ProfilePage';
-import LoginPage from '../containers/LoginPage';
-import CreateRequestPage from '../containers/CreateRequestPage';
+const MainPage = lazy(() => import('../containers/MainPage'));
+const ProtectedRoute = lazy(() => import('./ProtectedRoute'));
+const RequestPage = lazy(() => import('../containers/RequestPage'));
+const ProfilePage = lazy(() => import('../containers/ProfilePage'));
+const LoginPage = lazy(() => import('../containers/LoginPage'));
+const CreateRequestPage = lazy(() => import('../containers/CreateRequestPage'));
 
 class Routes extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route path="/login" component={LoginPage} />
-        <ProtectedRoute path="/request" component={RequestPage} auth={this.props.isAuthenticated} />
-        <ProtectedRoute path="/profile" component={ProfilePage} auth={this.props.isAuthenticated} />
-        <ProtectedRoute path="/create" component={CreateRequestPage} auth={this.props.isAuthenticated} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/login" component={LoginPage} />
+          <ProtectedRoute path="/request" component={RequestPage} auth={this.props.isAuthenticated} />
+          <ProtectedRoute path="/profile" component={ProfilePage} auth={this.props.isAuthenticated} />
+          <ProtectedRoute path="/create" component={CreateRequestPage} auth={this.props.isAuthenticated} />
+        </Switch>
+      </Suspense>
     );
   }
 }
